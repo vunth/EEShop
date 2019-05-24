293dd52754aa7779ef358350901a2cc33094ebfb
Vu Nguyen <hoangvu.nguyen@evizi.com>
2019-05-24 15:28:06 +0700

    Layout tabbar items

--- components/Main/Shop/Shop.js
+++ components/Main/Shop/Shop.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 import Home from './Home/Home';
 import Cart from './Cart/Cart';
 import Search from './Search/Search';
 import Contact from './Contact/Contact';
 import Header from './Header';
+import homeIconS from '../../../media/appIcon/home.png';
+import homeIcon from '../../../media/appIcon/home0.png';
+import cartIconS from '../../../media/appIcon/cart.png';
+import cartIcon from '../../../media/appIcon/cart0.png';
+import searchIconS from '../../../media/appIcon/search.png';
+import searchIcon from '../../../media/appIcon/search0.png';
+import contactIconS from '../../../media/appIcon/contact.png';
+import contactIcon from '../../../media/appIcon/contact0.png';
 
 class Shop extends React.Component {
     constructor(props) {
@@ -17,27 +25,17 @@ class Shop extends React.Component {
         open();
     }
     render() {
-        // const { open } = this.props;
+        const { iconStyle } = styles;
         return (
-            // <View style={styles.container}>
-            //     <Text style={styles.welcome}>Welcome to Shop component!</Text>
-            //     <Button
-            //         title="Open Menu By Button"
-            //         onPress={() => open()}
-            //     />
-            // <TouchableOpacity onPress={this.openMenu.bind(this)}>
-            //     <Text>Open Menu By Touchable</Text>
-            // </TouchableOpacity>
-            // </View>
             <View style={styles.container}>
                 <Header onOpen={this.openMenu.bind(this)} />
                 <TabNavigator>
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'home'}
                         title="Home"
-                        // renderIcon={() => <Image source={...} />}
-                        // renderSelectedIcon={() => <Image source={...} />}
-                        badgeText="1"
+                        renderIcon={() => <Image source={homeIcon} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image source={homeIconS} style={iconStyle} />}
+                        selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
                         onPress={() => this.setState({ selectedTab: 'home' })}
                     >
                         <Home />
@@ -45,9 +43,10 @@ class Shop extends React.Component {
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'cart'}
                         title="Cart"
-                        // renderIcon={() => <Image source={...} />}
-                        // renderSelectedIcon={() => <Image source={...} />}
-                        // renderBadge={() => <CustomBadgeView />}
+                        renderIcon={() => <Image source={cartIcon} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image source={cartIconS} style={iconStyle} />}
+                        badgeText="1"
+                        selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
                         onPress={() => this.setState({ selectedTab: 'cart' })}
                     >
                         <Cart />
@@ -55,9 +54,9 @@ class Shop extends React.Component {
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'search'}
                         title="Search"
-                        // renderIcon={() => <Image source={...} />}
-                        // renderSelectedIcon={() => <Image source={...} />}
-                        // renderBadge={() => <CustomBadgeView />}
+                        renderIcon={() => <Image source={searchIcon} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image source={searchIconS} style={iconStyle} />}
+                        selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
                         onPress={() => this.setState({ selectedTab: 'search' })}
                     >
                         <Search />
@@ -65,9 +64,9 @@ class Shop extends React.Component {
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'contact'}
                         title="Contact"
-                        // renderIcon={() => <Image source={...} />}
-                        // renderSelectedIcon={() => <Image source={...} />}
-                        // renderBadge={() => <CustomBadgeView />}
+                        renderIcon={() => <Image source={contactIcon} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image source={contactIconS} style={iconStyle} />}
+                        selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
                         onPress={() => this.setState({ selectedTab: 'contact' })}
                     >
                         <Contact />
@@ -85,14 +84,8 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    iconStyle: {
+        width: 20,
+        height: 20,
     },
 });
